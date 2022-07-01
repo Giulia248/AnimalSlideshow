bfcf87b13d3dedc7ccef71b19465959c450f7661
Giulia <giuliafloris88@gmail.com>
2022-07-01 10:14:54 +0200

    Initial Commit

---
+++ AnimalSlideshow/AnimalSlideshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalSlideshowApp.swift
+//  AnimalSlideshow
+//
+//  Created by user on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimalSlideshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
